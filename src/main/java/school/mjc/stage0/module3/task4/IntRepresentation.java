de4f034a2fb6f1971540486f4ce09469b3192ec2
Vu <nguyentuanvu9x@gmail.com>
2023-08-13 23:06:18 +0300

    Added: IntRepresentation.java

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        int intC = c;
+        int intH = h;
+        int intA = a;
+        int intR = r;
+
+        System.out.println(intC);
+        System.out.println(intH);
+        System.out.println(intA);
+        System.out.println(intR);
     }
 }
