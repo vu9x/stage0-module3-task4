1fe951ef57110f6be2d197fef36f9d62be451621
Vu <nguyentuanvu9x@gmail.com>
2023-08-14 22:32:48 +0300

    Adding: FormulaCreator.java

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int i = (9 * a * a - 5 * b + 2  + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(i);
     }
 }
